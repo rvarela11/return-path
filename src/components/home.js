17fc91bcf7b7a69590893f39016c1841a9ce3fb8
Robert Varela <robert.varela@rvarela-ret.local>
2017-03-04 20:01:35 -0600

    cleaned up code

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,8 @@ class Home extends Component {
   /*--------------- componentWillMount function -----------------*/
   /*-------------------------------------------------------------*/
 
-  /*--------------- Get Users from data array and push them to specific arrays -----------------*/
+  /* This function will gather the user.group data from the data array and push
+     them to specific arrays. */
 
   componentWillMount () {
     const all_data = this.state.data;
@@ -14,7 +15,7 @@ class Home extends Component {
     const sales_array = this.state.sales;
     const support_array = this.state.support;
 
-    all_data.map((user) => {
+    all_data.forEach((user) => {
       if (user.group === "IT") {
         it_array.push(user);
         this.setState({it_array});
@@ -28,7 +29,6 @@ class Home extends Component {
        this.setState({support_array});
      }
     });
-
   }
 
 constructor (props) {
@@ -87,16 +87,161 @@ constructor (props) {
       support: [],
       results: [],
       temp_user: [],
-      search: "",
       password: "button",
       button: "All"
     }
     this.searchUpdated = this.searchUpdated.bind(this);
     this.results = this.results.bind(this);
     this.search_results = this.search_results.bind(this);
-    this.group = this.group.bind(this);
-    this.group_display = this.group_display.bind(this);
+    this.colorButton = this.colorButton.bind(this);
+    this.display = this.display.bind(this);
     this.bodyHideSearchResults = this.bodyHideSearchResults.bind(this);
+  }
+
+  /*-----------------------------------------------*/
+  /*------------------ Render ---------------------*/
+  /*-----------------------------------------------*/
+
+  render() {
+
+    return <div>
+      <header></header>
+
+        <section className="color-buttons">
+            <div className="color-buttons__top--selected"></div>
+            <ul>
+              <li onClick={this.colorButton}>All</li>
+              <li onClick={this.colorButton}>IT</li>
+              <li onClick={this.colorButton}>Sales</li>
+              <li onClick={this.colorButton}>Support</li>
+            </ul>
+        </section>
+
+        <section>
+          <form className="search-form">
+              <input type="text" className="search-form__input" placeholder="Name" onChange={this.searchUpdated}/>
+            <div>{this.search_results()}</div>
+          </form>
+        </section>
+
+        <section>
+          <div>{this.display()}</div>
+        </section>
+    </div>
+  }
+
+  /*------------------------------------------------------*/
+  /*--------------- ColorButton function -----------------*/
+  /*-----------------------------------------------------*/
+
+  // This function runs everytime a colored button is clicked.
+  // This is a 2 step function.
+
+  colorButton (e) {
+
+    this.setState({temp_user: []});
+    this.setState({password: "button"});
+
+    // Step 1 = Change button color when clicked
+
+    const button_name = e.target.innerHTML;
+    const button_color = window.getComputedStyle(e.target).backgroundColor;
+    const all_buttons_li = document.querySelectorAll(".color-buttons ul li");
+    const button_top = document.querySelector(".color-buttons__top--selected");
+
+      if (button_color == "rgba(0, 0, 0, 0)" ) {
+        all_buttons_li.forEach(button => {
+            button.style.backgroundColor = "rgba(0, 0, 0, 0)";
+      })
+
+      if (button_name === "All") {
+        e.target.style.backgroundColor = "#cf196f";
+        button_top.style.backgroundColor = "#cf196f";
+      }
+      else if (button_name === "IT") {
+        e.target.style.backgroundColor = "#3333FF";
+        button_top.style.backgroundColor = "#3333FF";
+      }
+      else if (button_name === "Sales") {
+        e.target.style.backgroundColor = "#ffc600";
+        button_top.style.backgroundColor = "#ffc600";
+      }
+      else if (button_name === "Support") {
+        e.target.style.backgroundColor = "green";
+        button_top.style.backgroundColor = "green";
+      }
+    }
+
+    // Step 2 = Display the right info
+
+    const filters = ["All", "IT", "Sales", "Support"];
+    filters.forEach((filter) => {
+      if (filter === button_name) {
+        this.setState({button: filter});
+      }
+    });
+  }
+
+  /*--------------------------------------------------*/
+  /*--------------- display function -----------------*/
+  /*--------------------------------------------------*/
+
+  display () {
+
+    const temp_user = this.state.temp_user;
+
+    const password = this.state.password;
+
+    if (password === "button") {
+      let user_display_results = [];
+      let button_name = this.state.button;
+
+      if (button_name === "All") {
+        user_display_results = this.state.data;
+      } else if (button_name === "IT") {
+        user_display_results = this.state.it;
+      } else if (button_name === "Sales") {
+        user_display_results = this.state.sales;
+      } else if (button_name === "Support") {
+        user_display_results = this.state.support;
+      }
+
+      return <div>
+                <h2 className="display__title">{button_name}</h2>
+                <ul className="display__ul">
+                  {user_display_results.map((user, index)=> {
+                    return <div key={index}>
+                        <li onClick={this.singleUserInfo.bind(this, user, index)}>
+                          <span style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
+                          <span>{`${user.first_name} ${user.last_name}`}</span>
+                          <span className="display_span_region">{`${user.region}`}</span>
+                        </li>
+                    </div>
+                  })}
+                </ul>
+            </div>
+    } else {
+      return <div>
+                <h2 className="display__title">User</h2>
+                <ul className="display__ul single-user__ul">
+                {temp_user.map((user, index)=> {
+                  return <div key={index}>
+                            <li>
+                              <span style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
+                              <span>{`${user.first_name} ${user.last_name}`}</span>
+                              <span>{`${user.region}`}</span>
+                            </li>
+                            <div className="single-user__buttons">
+                              <p>Add to:</p>
+                              <p onClick={this.addUser.bind(this, user, index)}>IT</p>
+                              <p onClick={this.addUser.bind(this, user, index)}>Sales</p>
+                              <p onClick={this.addUser.bind(this, user, index)}>Support</p>
+                            </div>
+                        </div>
+                })}
+                </ul>
+            </div>
+    }
   }
 
 /*------------------------------------------------------*/
@@ -104,16 +249,13 @@ constructor (props) {
 /*------------------------------------------------------*/
 
   searchUpdated (event) {
+    this.setState({temp_user: []});
+    const resultsArray = this.results(event.target.value);
+    this.setState({results: resultsArray});
     document.body.addEventListener("click", this.bodyHideSearchResults);
-
-      this.setState({search: event.target.value});
-      this.setState({temp_user: []});
-      const resultsArray = this.results();
-      this.setState({results: resultsArray});
   }
 
-  results () {
-    const wordToMatch = this.state.search;
+  results (wordToMatch) {
     const data  = this.state.data;
 
     return data.filter(name => {
@@ -122,23 +264,16 @@ constructor (props) {
       });
   }
 
-  hideSearchResults (event) {
-    event.preventDefault();
-    this.setState({results: []});
-  }
+  bodyHideSearchResults (e) {
 
-  bodyHideSearchResults (event) {
-
-    /*--------------- Clean input search -----------------*/
-
-    const search_input = document.querySelector(".search");
+    // Clean input search
+    const search_input = document.querySelector(".search-form__input");
     search_input.value = "";
-    this.setState({search: ""});
 
-    /*--------------- Hide search results when user clicks outside -----------------*/
-
+    // Hide search results when user clicks outside
     const search_li_body = event.target.parentElement.classList[0];
     const search_li_class = document.querySelectorAll(".search_ul_li");
+    console.log(search_li_body);
 
     search_li_class.forEach(li_class => {
       if (search_li_body === li_class.classList[0]) {
@@ -150,16 +285,17 @@ constructor (props) {
     })
   }
 
+  // This function will display the search results
 
   search_results () {
     return (
-      <ul className="search_ul">
+      <ul className="display__ul search-form-results__ul">
         {this.state.results.map((user, index)=> {
           return <div key={index}>
-            <li className="search_ul_li" onClick={this.userInfo.bind(this, user, index)}>
-              <span className="display_span_line" style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
+            <li className="search_ul_li" onClick={this.singleUserInfo.bind(this, user, index)}>
+              <span style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
               <span>{`${user.first_name} ${user.last_name}`}</span>
-              <span className="display_span_region">{`${user.region}`}</span>
+              <span>{`${user.region}`}</span>
             </li>
           </div>
         })}
@@ -167,16 +303,17 @@ constructor (props) {
     )
   }
 
-  /*-------------------------------------------------------------------------------*/
-  /*--------------- userInfo function - cont. on group_display () -----------------*/
-  /*-------------------------------------------------------------------------------*/
+  /*------------------------------------------------*/
+  /*--------------- singleUserInfo -----------------*/
+  /*------------------------------------------------*/
 
-  userInfo (user, index, event) {
+  singleUserInfo (user, index, event) {
 
     this.setState({results: []});
 
-    const all_buttons_li = document.querySelectorAll(".group_buttons_li");
-    const div_button_top = document.querySelector(".div_button_color_selected");
+    const all_buttons_li = document.querySelectorAll(".color-buttons ul li");
+    const div_button_top = document.querySelector(".color-buttons__top--selected");
+
     div_button_top.style.backgroundColor = "#cf196f";
 
     all_buttons_li.forEach(button => {
@@ -187,165 +324,6 @@ constructor (props) {
     temp_user_userInfo.push(user);
     this.setState({temp_user_userInfo});
     this.setState({password: "item"});
-  }
-
-  /*------------------------------------------------*/
-  /*--------------- Group function -----------------*/
-  /*------------------------------------------------*/
-
-  group (e) {
-
-    this.setState({temp_user: []});
-    this.setState({password: "button"});
-
-    /*--------------- Change button color when clicked -----------------*/
-    const button_name = e.target.innerHTML;
-    const button_color = window.getComputedStyle(e.target).backgroundColor;
-    const all_buttons_li = document.querySelectorAll(".group_buttons_li");
-    const div_button_top = document.querySelector(".div_button_color_selected");
-    if (button_color == "rgba(0, 0, 0, 0)" ) {
-      all_buttons_li.forEach(button => {
-          button.style.backgroundColor = "rgba(0, 0, 0, 0)";
-      })
-
-      if (button_name === "All") {
-        e.target.style.backgroundColor = "#cf196f";
-        div_button_top.style.backgroundColor = "#cf196f";
-      }
-      else if (button_name === "IT") {
-        e.target.style.backgroundColor = "#3333FF";
-        div_button_top.style.backgroundColor = "#3333FF";
-      }
-      else if (button_name === "Sales") {
-        e.target.style.backgroundColor = "#ffc600";
-        div_button_top.style.backgroundColor = "#ffc600";
-      }
-      else if (button_name === "Support") {
-        e.target.style.backgroundColor = "green";
-        div_button_top.style.backgroundColor = "green";
-      }
-    }
-
-    /*--------------- Display the right info -----------------*/
-
-    const filters = ["All", "IT", "Sales", "Support"];
-    filters.forEach((filter) => {
-      if (filter === button_name) {
-        this.setState({button: filter});
-      }
-    });
-
-    /*--------------- Pushing from Data Array into individual arrays -----------------*/
-
-    const all_data = this.state.data;
-    const it_array = this.state.it;
-    const sales_array = this.state.sales;
-    const support_array = this.state.support;
-    let it_count = 0;
-    let sales_count = 0;
-    let support_count = 0;
-
-    all_data.map((user) => {
-
-      /*--------------- Only run if the button was clicked -----------------*/
-
-      if (button_name === "IT") {
-        if (user.group === "IT") {
-            it_count++
-        }
-        if (it_array.length < it_count) {
-          if (button_name  === "IT" && user.group === "IT") {
-            it_array.push(user);
-            this.setState({it_array});
-          }
-        }
-      }
-
-      if (button_name === "Sales") {
-        if (user.group === "Sales") {
-          sales_count++
-        }
-        if (sales_array.length < sales_count) {
-          if (button_name  === "Sales" && user.group === "Sales") {
-            sales_array.push(user);
-            this.setState({sales_array});
-          }
-        }
-      }
-
-      if (button_name === "Support") {
-        if (user.group === "Support") {
-          support_count++
-        }
-        if (support_array.length < support_count) {
-          if (button_name  === "Support" && user.group === "Support") {
-            support_array.push(user);
-            this.setState({support_array});
-          }
-        }
-      }
-    });
-  }
-
-  /*--------------------------------------------------------*/
-  /*--------------- group_display function -----------------*/
-  /*--------------------------------------------------------*/
-
-  group_display () {
-
-    const temp_user = this.state.temp_user;
-    const password = this.state.password;
-
-    if (password === "button") {
-      let group_display_results = [];
-      let button_name = this.state.button;
-
-      if (button_name === "All") {
-        group_display_results = this.state.data;
-      } else if (button_name === "IT") {
-        group_display_results = this.state.it;
-      } else if (button_name === "Sales") {
-        group_display_results = this.state.sales;
-      } else if (button_name === "Support") {
-        group_display_results = this.state.support;
-      }
-
-      return <div>
-        <h2 className="display_title">{button_name}</h2>
-        <ul className="display_ul">
-        {group_display_results.map((user, index)=> {
-          return <div key={index}>
-              <li onClick={this.userInfo.bind(this, user, index)}>
-                <span className="display_span_line" style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
-                <span>{`${user.first_name} ${user.last_name}`}</span>
-                <span className="display_span_region">{`${user.region}`}</span>
-              </li>
-          </div>
-        })}
-      </ul>
-      </div>
-    } else {
-      return <div>
-        <h2 className="display_title">User</h2>
-        <ul className="display_ul user_display_ul">
-        {temp_user.map((user, index)=> {
-          return <div key={index}>
-              <li>
-                <span className="display_span_line" style={{background: (user.group === "IT") ? '#3333FF' : (user.group === "Sales") ? '#ffc600' : (user.group === "Support") ? 'green' : 'black'}}></span>
-                <span>{`${user.first_name} ${user.last_name}`}</span>
-                <span className="display_span_region">{`${user.region}`}</span>
-              </li>
-              <div className="user_div_buttons">
-                <p>Add to:</p>
-                <p onClick={this.addUser.bind(this, user, index)} className="user_p_buttons user_button_it">IT</p>
-                <p onClick={this.addUser.bind(this, user, index)} className="user_p_buttons user_button_sales">Sales</p>
-                <p onClick={this.addUser.bind(this, user, index)} className="user_p_buttons user_button_support">Support</p>
-              </div>
-          </div>
-        })}
-      </ul>
-      </div>
-    }
   }
 
   /*------------------------------------------*/
@@ -379,7 +357,6 @@ constructor (props) {
       data_group_array = this.state.it;
       data_group_array.push(user);
       this.setState({data_group_array});
-      this.group_display();
     } else if (user_button === "Sales") {
       user.group = "Sales";
       data_group_array = this.state.sales;
@@ -391,35 +368,6 @@ constructor (props) {
       data_group_array.push(user);
       this.setState({data_group_array});
     }
-  }
-
-  /*-----------------------------------------------*/
-  /*------------------ render ---------------------*/
-  /*-----------------------------------------------*/
-
-  render() {
-
-    return <div>
-      <header className="header_background_image"></header>
-
-        <div className="div_group_buttons">
-          <div className="div_button_color_selected"></div>
-          <ul className="group_buttons_ul">
-            <li className="group_buttons_li button_all" onClick={this.group}>All</li>
-            <li className="group_buttons_li button_it" onClick={this.group}>IT</li>
-            <li className="group_buttons_li button_sales" onClick={this.group}>Sales</li>
-            <li className="group_buttons_li button_support" onClick={this.group}>Support</li>
-          </ul>
-        </div>
-
-        <form className="search-form">
-            <input type="text" className="search" placeholder="Name" onChange={this.searchUpdated}/>
-            {/* <button className="close_search_results" onClick={this.hideSearchResults.bind(this)}> Hide Search Results</button> */}
-          <div>{this.search_results()}</div>
-        </form>
-
-      <div className="group_display">{this.group_display()}</div>
-    </div>
   }
 }
 
